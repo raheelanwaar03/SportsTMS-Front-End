e8f99183fa1398c7fd86ebd9801dc3601241a1d6
raheelanwaar03 <raheelanwaar0316@gmail.com>
2026-04-28 18:53:32 +0200

    changes in code of api class

--- ApiServices.js
+++ ApiServices.js
@@ -2,9 +2,7 @@
 export const AUTHENTICATION_API = {
   BASE_URL: "http://localhost:5000/api",
   LOGIN: "/users/login",
-  REGISTER: "/users/register",
-  FORGOT_PASSWORD: "/auth/forgot-password",
-  ALL_USERS: "/users"
+  REGISTER: "/users/register"
 };
 
 // Tournament Services BaseUrl API
@@ -14,8 +12,7 @@ export const TOURNAMENT_API = {
   GET_ALL_TOURNAMENTS: "/",
   GET_TOURNAMENT: "/:id",
   UPDATE_TOURNAMENT: "/:id",
-  DELETE_TOURNAMENT: "/:id",
-  GET_TOURNAMENTS_BY_ORGANIZER: "/organizer/:organizerId"
+  DELETE_TOURNAMENT: "/:id"
 };
 
 // Team Services BaseUrl API
@@ -26,10 +23,7 @@ export const TEAM_API = {
   GET_TEAM: "/:id",
   UPDATE_TEAM: "/:id",
   DELETE_TEAM: "/:id",
-  ADD_PLAYER: "/:id/players",
-  REMOVE_PLAYER: "/:id/players/:playerId",
-  GET_TEAM_PLAYERS: "/:id/players",
-  ASSIGN_TO_TOURNAMENT: "/:id/tournament/:tournamentId"
+  ADD_PLAYER: "/:id/players"
 };
 
 // Match Services BaseUrl API
@@ -38,28 +32,19 @@ export const MATCH_API = {
   SCHEDULE_MATCH: "/",
   GET_ALL_MATCHES: "/",
   GET_MATCH: "/:id",
-  UPDATE_MATCH: "/:id",
-  DELETE_MATCH: "/:id",
-  UPDATE_RESULT: "/:id/result",
-  GET_TOURNAMENT_MATCHES: "/tournament/:tournamentId",
-  GET_TEAM_MATCHES: "/team/:teamId"
+  UPDATE_MATCH_RESULT: "/:id/result"
 };
 
 // Standings Services BaseUrl API
 export const STANDINGS_API = {
   BASE_URL: "http://localhost:9000/api/standings",
-  GET_TOURNAMENT_STANDINGS: "/:tournamentId",
-  GET_TEAM_POSITION: "/:tournamentId/team/:teamId",
-  REFRESH_STANDINGS: "/:tournamentId/refresh"
+  GET_TOURNAMENT_STANDINGS: "/:tournamentId"
 };
 
-// Announcements BaseUrl API
+// Announcements Services BaseUrl API
 export const ANNOUNCEMENTS_API = {
   BASE_URL: "http://localhost:10000/api/announcements",
   CREATE_ANNOUNCEMENT: "/",
   GET_ALL_ANNOUNCEMENTS: "/",
-  GET_ANNOUNCEMENT: "/:id",
-  UPDATE_ANNOUNCEMENT: "/:id",
-  DELETE_ANNOUNCEMENT: "/:id",
-  GET_TOURNAMENT_ANNOUNCEMENTS: "/tournament/:tournamentId"
+  GET_ANNOUNCEMENT: "/:id"
 };
